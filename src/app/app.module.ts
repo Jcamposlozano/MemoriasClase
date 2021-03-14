ae7ea2f845b7924390885009b243c05569930915
Jonathan Campos <jc2222@gmail.com>
2021-03-14 18:59:35 -0500

    Se actualiza a la clase 2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BasesdedatosComponent } from './component/basesdedatos/basesdedatos.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BasesdedatosComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/component/basesdedatos/basesdedatos.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-basesdedatos',
+  templateUrl: './basesdedatos.component.html',
+  styles: [
+  ]
+})
+export class BasesdedatosComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
